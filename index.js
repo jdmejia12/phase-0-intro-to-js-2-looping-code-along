ca8029210183ce8f1db83426ac2917591cf5e71b
JD <jdmejia712@gmail.com>
2021-09-20 00:23:42 -0400

    assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+
+function writeCards(names, event){
+    const theseNames = []
+    for(let i = 0; i<names.length;i++){
+      console.log(`Wrapped ${names[i]} and added a bow!`);
+      theseNames.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+      }
+      
+    return theseNames;
+  }
+  
+  
+
+function countDown(num){
+    let i = num;
+    while (i>=0){
+    console.log(i);
+      i--; // the iteration moves INSIDE the body of the loop!
+    }
+  }
+  countDown(10)
